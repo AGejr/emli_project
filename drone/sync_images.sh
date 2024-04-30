168a8f3eeead2c897d79e5966ea1285745d0c119
AGejr <albert.gejr@gmail.com>
2024-04-30 13:37:57 +0200

    added wifi logging to sync script

--- drone/sync_images.sh
+++ drone/sync_images.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
 
+function log_wifi_stats ()
+{
+  echo "Starting wifi logging"
+  while [[ true ]]; do
+    signal_level=$(cat /proc/net/wireless | grep $1 | tr -s ' ' | cut -d ' ' -f 4)
+    link_quality=$(cat /proc/net/wireless | grep $1 | tr -s ' ' | cut -d ' ' -f 3)
+    epoch_seconds=$(date +%s)
+    echo "$epoch_seconds - Signal level: $signal_level, Link quality $link_quality" >> ./wifi_qual.log
+    sleep 1
+  done
+}
+
 function sync_images {
   echo "Syncing images..."
   rsync -avz --progress emli@10.0.0.10:/home/emli/Images ./
   echo "Images synced"
   nmcli connection down EMLI-TEAM-16
+  kill $log_wifi_stats_pid
   exit 0
 }
 
@@ -15,12 +28,16 @@ while [[ true ]]; do
     echo "EMLI-TEAM-16 wifi visible"
     # If already connected to the network then sync images
     if [[ $(nmcli -t -f active,ssid dev wifi | grep '^yes' | cut -d ':' -f2) == 'EMLI-TEAM-16' ]]; then
+      log_wifi_stats wlp4s0 &
+      log_wifi_stats_pid=$!
       sync_images
     else
       # Connect to network and sync images
       nmcli device wifi connect EMLI-TEAM-16
       if [[ $? -eq 0 ]]; then
         echo "Connected to wifi"
+        log_wifi_stats wlp4s0 &
+        log_wifi_stats_pid=$!
         sync_images
       else
         echo "Could not connect to wifi"
@@ -29,3 +46,5 @@ while [[ true ]]; do
   fi
   sleep 1
 done
+
+trap "echo 'Stopping wifi logging'; kill $log_wifi_stats_pid" EXIT
