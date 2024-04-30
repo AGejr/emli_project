27818bd21224f4a8ef9bc7f3f3fb9b2230a3327d
AGejr <albert.gejr@gmail.com>
2024-04-30 15:21:51 +0200

    added metadata update to synced images

--- drone/sync_images.sh
+++ drone/sync_images.sh
@@ -12,13 +12,38 @@ function log_wifi_stats ()
   done
 }
 
-function sync_images {
+function sync_images ()
+{
   echo "Syncing images..."
   rsync -avz --progress emli@10.0.0.10:/home/emli/Images ./
   echo "Images synced"
-  nmcli connection down EMLI-TEAM-16
-  kill $log_wifi_stats_pid
-  exit 0
+}
+
+function sync_local_to_remote ()
+{
+  echo "Syncing local to remote..."
+  rsync -avz --progress ./Images/ emli@10.0.0.10:/home/emli/Images
+  echo "Files synced"
+
+}
+
+function update_metadata () 
+{
+  drone_id="WILDDRONE-001"
+  epoch_seconds=$(date +%s)
+  metadata_files=$(find ./Images -type f -name '*.json')
+  for metadata_file in $metadata_files
+  do
+    if jq '.["Drone Copy"]' "$metadata_file" | grep -q null; then
+      jq --arg droneID "$drone_id" --argjson epoch "$epoch_seconds" \
+        '.["Drone Copy"] = {"Drone ID": $droneID, "Seconds Epoch": $epoch}' \
+        "$metadata_file" > temp.json && mv temp.json "$metadata_file"
+      echo "Metadata updated for $metadata_file"
+    else
+      echo "Skipping $metadata_file"
+    fi
+  done
+  sync_local_to_remote  
 }
 
 while [[ true ]]; do 
@@ -31,6 +56,8 @@ while [[ true ]]; do
       log_wifi_stats wlp4s0 &
       log_wifi_stats_pid=$!
       sync_images
+      update_metadata
+      break 
     else
       # Connect to network and sync images
       nmcli device wifi connect EMLI-TEAM-16
@@ -39,6 +66,8 @@ while [[ true ]]; do
         log_wifi_stats wlp4s0 &
         log_wifi_stats_pid=$!
         sync_images
+        update_metadata
+        break
       else
         echo "Could not connect to wifi"
       fi
@@ -47,4 +76,7 @@ while [[ true ]]; do
   sleep 1
 done
 
+nmcli connection down EMLI-TEAM-16
+kill $log_wifi_stats_pid
 trap "echo 'Stopping wifi logging'; kill $log_wifi_stats_pid" EXIT
+exit 0
