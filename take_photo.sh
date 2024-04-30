a3a0e8de5cedbea81b615f7e6fe556904a347634
AGejr <albert.gejr@gmail.com>
2024-04-30 12:19:08 +0200

    added take photo script and mock version to test with

---
+++ take_photo.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+create_metadata ()
+{
+  echo "Extracting EXIF from ./$3/$2"
+  subject_distance=$(exiftool -SubjectDistance ./$3/$2 | awk -F': ' '{print $2}')
+  exposure_time=$(exiftool -ExposureTime ./$3/$2 | awk -F': ' '{print $2}')
+  iso=$(exiftool -ISO ./$3/$2 | awk -F': ' '{print $2}')
+  metadata="
+  {
+    \"File Name\": \"$2\",
+    \"Create Date\": \"$(date +"%Y-%m-%d %H:%M:%S.%3N%:z")\",
+    \"Create Seconds Epoch\": $(date +"%s.%3N"),
+    \"Trigger\": \"$1\",
+    \"Subject Distance\": $subject_distance,
+    \"Exposure Time\": \"$exposure_time\",
+    \"ISO\": $iso
+  }
+  "
+  echo "Create metadata"
+  metadata_filename=$(echo $2 | sed 's/jpg/json/')
+  echo $metadata | tee ./$3/$metadata_filename
+  echo "Saved metadata to ./$3/$metadata_filename"
+}
+
+trigger=$1
+
+folder_name=$(date +"%Y-%m-%d")
+if [[ ! -d ./$folder_name ]]; then
+  echo "Making dir $folder_name"
+  mkdir ./$folder_name
+fi
+
+file_name=$(date +"%H%M%S_%3N.jpg")
+file_path="./$folder_name/$file_name"
+
+rpicam-still -t 0.01 -o $file_path
+echo "Saving pic as $file_path"
+
+create_metadata $trigger $file_name $folder_name
+
