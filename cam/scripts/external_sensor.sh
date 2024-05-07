4f1faa99afee54de13db75339166cb09d897ee04
qnudsgaard <cknud20@student.sdu.dk>
2024-05-07 14:43:56 +0200

    added external_sensor script
    quick fix script
    
    made script exec

---
+++ cam/scripts/external_sensor.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# MQTT Broker settings
+BROKER_IP="10.0.0.10"
+TOPIC="/trigger/external"
+
+# Path to the script
+SCRIPT="/home/emli/Git/emli_project/cam/scripts/take_photo.sh external"
+
+# Function to handle messages
+message_received() {
+    echo "Message received on $TOPIC, running take_photo.sh"
+    $SCRIPT
+}
+
+# Loop to listen for mqtt messages
+mosquitto_sub -h $BROKER_IP -t $TOPIC | while read msg
+do
+    message_received "$msg"
+done
