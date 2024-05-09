16858554e96c85edfb0f5549ecf662603897e1f4
qnudsgaard <cknud20@student.sdu.dk>
2024-05-09 12:41:45 +0200

    updated take_pic scripts

--- cam/scripts/take_photo_each_second.sh
+++ cam/scripts/take_photo_each_second.sh
@@ -3,11 +3,11 @@
 BASE_DIR=$(pwd)
 
 take_photo() {
-    local output=$(./take_photo.sh motion 2>&1)
+    local output=$(./home/emli/Git/emli_project/cam/scripts/take_photo.sh motion 2>&1)
     echo "$output"
     local dir_line=$(echo "$output" | grep 'Making dir')
     if [[ $dir_line =~ Making\ dir\ (.*) ]]; then
-        photo_directory="${BASE_DIR}/${BASH_REMATCH[1]}"  # Use full path for clarity
+        photo_directory="/home/emli/Git/emil_project/cam/temp"
         echo "Using directory: $photo_directory"
     fi
 }
@@ -26,7 +26,7 @@ detect_motion() {
     echo "Image 1: $img1"
     echo "Image 2: $img2"
 
-    local motion_result=$(python3 ../motion_detect/motion_detect.py "$img1" "$img2")
+    local motion_result=$(python3 /home/emli/Git/emli_project/cam/motion_detect/motion_detect.py "$img1" "$img2")
     echo "$motion_result"
 }
 
@@ -34,18 +34,37 @@ while true; do
     take_photo
     sleep 1
     if [ $(ls $photo_directory/*.jpg | wc -l) -ge 2 ]; then
+        newest=$(ls -t $photo_directory/*.jpg | head -n 1)
+        newest_json="${newest%.jpg}.json"
+
+        oldest=$(ls $photo_directory/*.jpg -t | tail -n 1)
+        oldest_json="${oldest%.jpg}.json"
+
         motion_output=$(detect_motion)
         echo "$motion_output"
         if [[ "$motion_output" == *"Motion detected"* ]]; then
-            echo "Motion detected - keeping images."
-            # In this case, keep both images and do nothing else
+	    
+        echo "Motion detected - keeping image"
+            
+	    cp "$newest" "/home/emli/Git/emli_project/images"
+	    cp "$newest_json" "/home/emli/Git/emli_project/images"
+
+        echo "Images and json had been coopied"    
+
+        echo "Removing oldest image: $oldest"
+            rm -- "$oldest"
+            # Remove the corresponding JSON file
+            if [ -f "$oldest_json" ]; then
+                rm -- "$oldest_json"
+                echo "Removed metadata file: $oldest_json"
+            fi
+            
+	    # In this case, keep both images and do nothing else
         else
             # If no motion detected, delete the oldest image
-            oldest=$(ls $photo_directory/*.jpg -t | tail -n 1)
             echo "Removing oldest image: $oldest"
             rm -- "$oldest"
             # Remove the corresponding JSON file
-            oldest_json="${oldest%.jpg}.json"
             if [ -f "$oldest_json" ]; then
                 rm -- "$oldest_json"
                 echo "Removed metadata file: $oldest_json"
@@ -54,3 +73,4 @@ while true; do
         fi
     fi
 done
+???END
